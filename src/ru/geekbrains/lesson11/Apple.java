37423544590033f7cd20f2b6da4ba945b63270ee
MaryamHub <rmr87@yandex.ru>
2021-03-01 18:31:08 +0300

    Commit

---
+++ src/ru/geekbrains/lesson11/Apple.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson11;
+
+class Apple extends Fruit {
+
+    Apple() {
+        this.setWeight(1.0f);
+    }
+}
---
+++ src/ru/geekbrains/lesson11/Fruit.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.lesson11;
+
+abstract class Fruit {
+
+    private float weight;
+
+    void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+
+    Fruit(){
+    }
+}
